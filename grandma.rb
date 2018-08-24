cda343b8377044ac89aa5b1ec639db3597ba0f66
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 06:36:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(var1)
   if var1 = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if var1 != var1.upcase()
+  elsif var1 != var1.upcase()
      return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
